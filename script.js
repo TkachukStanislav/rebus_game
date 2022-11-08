a38e641fc7b2d91344793fe57f3aff184e7e13ec
senivan <>
2022-11-08 12:38:36 +0200

    Зробив завдання

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let input1 = document.getElementById('task1'),
+res = document.getElementById('result'),
+ button = document.getElementById('button'),
+ counter = 0;
+
+button.addEventListener('click', 
+ function (){
+    if(input1.value == 'Github'){
+        counter++
+        button.setAttribute("disabled", "true")
+        res++
+     }
+     else{
+        alert('false')
+     }
+    
+ }
+)
 
